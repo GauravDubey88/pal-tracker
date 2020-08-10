a2ec48f4a9e77004d1b61400a2ec9e3bb7c8a97f
student <RakshaIsrani@dell.com>
2020-08-10 15:17:05 +0100

    configuring an app lab commit

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,23 +1,22 @@
 package io.pivotal.pal.tracker;
 
-
-
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.RestController;
-
-
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
-
 public class WelcomeController {
 
+    String welcomeMessage = null;
+
+    public WelcomeController(@Value("${welcome.message}") String Message){
+        welcomeMessage = Message;
+    }
 
     @GetMapping("/")
-
     public String sayHello() {
 
-        return "hello";
+        return welcomeMessage;
 
     }
 
